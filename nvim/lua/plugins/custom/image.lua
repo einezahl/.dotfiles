a3f8cff30a398f7fe1004b7678f7d9e08218974b
Tom Reclik <reclik@imm.rwth-aachen.de>
2026-05-05 16:27:35 +0200

    Switch image.nvim to kitty backend with markdown rendering

--- nvim/lua/plugins/custom/image.lua
+++ nvim/lua/plugins/custom/image.lua
@@ -1,7 +1,21 @@
 return {
   '3rd/image.nvim',
   build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+  lazy = false,
   opts = {
+    backend = 'kitty',
     processor = 'magick_cli',
+    hijack_file_patterns = { '*.png', '*.jpg', '*.jpeg', '*.gif', '*.webp', '*.avif' },
+    integrations = {
+      markdown = {
+        enabled = true,
+        clear_in_insert_mode = false,
+        download_remote_images = true,
+        only_render_image_at_cursor = false,
+        filetypes = { 'markdown', 'vimwiki' },
+      },
+    },
+    max_width_window_percentage = 100,
+    max_height_window_percentage = 90,
   },
 }
